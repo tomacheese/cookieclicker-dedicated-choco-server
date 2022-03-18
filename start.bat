486be79e714a914204b9ea1ea42b31fc9eb9591b
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-03-19 02:46:11 +0900

    fix: 全体スタートのパス修正

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 
-start "web-controller-server" /D "web-controller-server" "web-controller-server\start.bat"
-start "auto-start-clicker" /D "auto-start-clicker" "auto-start-clicker\checkInGame.bat"
-start "JQuake-Installer" /D "JQuake-Installer" "jquake\installer.bat"
+start "web-controller-server" /D "web-controller-server" "start.bat"
+start "auto-start-clicker" /D "auto-start-clicker" "checkInGame.bat"
+start "JQuake-Installer" /D "JQuake-Installer" "installer.bat"
