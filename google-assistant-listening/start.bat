8cbc20f61f17037367c01c7b190184fb41497316
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-05-09 20:00:03 +0900

    fix: https

--- google-assistant-listening/start.bat
+++ google-assistant-listening/start.bat
@@ -1,3 +1,3 @@
 @echo off
-start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "https:///localhost:7778"
+start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "http:///localhost:7778"
 php -S 0.0.0.0:7778
