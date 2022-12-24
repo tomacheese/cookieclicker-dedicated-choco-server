060331c0f34c10ae5b0640a153c7dc07f28cb43f
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-12-24 09:47:21 +0900

    fix: externalやめる

--- start.bat
+++ start.bat
@@ -1,14 +1,8 @@
 @echo off
 git pull
 
-DisplaySwitch.exe /external
-
 start "web-controller-server" /D "web-controller-server" /MIN "start.bat"
 start "google-assistant-listening" /D "google-assistant-listening" /MIN "start.bat"
 start "auto-start-clicker" /D "auto-start-clicker" /MIN "checkInGame.bat"
 start "JQuake-Installer" /D "jquake" /MIN "installer.bat"
 start "VbWinPos" /D "VbWinPos" /MIN "VbWinPos.exe"
-
-timeout /NOBREAK 10
-
-DisplaySwitch.exe /external
